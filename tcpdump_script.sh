08b70ecfcdf8d5ef4b0f21e904faf38797dc8482
Lyreplus <lyreplus@gmail.com>
2024-03-23 20:50:15 +0100

    fix: fixed tcpdump files creation, add csv parsing

--- tcpdump_script.sh
+++ tcpdump_script.sh
@@ -4,6 +4,6 @@ HOSTS=4
 
 for ((i = 1; i <= $HOSTS; i++)); do
   echo "starting host_$i"
-  sudo tcpdump -i s1-eth${i} > ./dumps/h${i}_traffic &
+  sudo tcpdump -i s1-eth${i} -w ./dumps/h${i}_traffic.pcap -U &
 done
 
